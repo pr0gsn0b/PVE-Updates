bed49a8d3676988212ac0d0422ea20afc9978ae9
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-06 13:52:24 -0600

    Create pve1-updates.sh

---
+++ pve1-updates.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+pve1hosts=( "tunnel1" "avery-kuma" "cockpit" "sabnzbd" "qbit" "tdarr" "overseerr" "plex" "servarr" "minecraft" )
+
+
+
+: <<'CMNT'
+for host in ${pve1hosts[@]}; do
+    ssh-copy-id root@$host.local
+done
+CMNT
+
+
+
+for host in ${pve1hosts[@]}; do
+    ssh -tt root@$host.local <<'CMD'
+    if grep -iq debian /etc/os-release > /dev/null 2>&1; then
+        apt update && apt upgrade -y
+    elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
+        apk update && apk upgrade
+    fi
+
+    if command -v docker > /dev/null 2>&1; then
+        for ct in /home/*/; do
+            cd "$ct"
+            docker compose pull
+            docker compose up -d
+        done
+    fi
+    docker image prune -f
+    exit
+CMD
+done
+
+ssh root@pve1.local 'apt update && apt dist-upgrade -y'
