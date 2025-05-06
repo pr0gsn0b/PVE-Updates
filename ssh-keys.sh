99974c2e37bbc328797f6341d0d71d1a378eb0bf
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-06 14:25:41 -0600

    Update ssh-keys.sh

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -3,3 +3,13 @@
 pve1hosts=( "tunnel1" "avery-kuma" "cockpit" "sabnzbd" "qbit" "tdarr" "overseerr" "plex" "servarr" "minecraft" )
 pve2hosts=( "homepage" "whoogle" "firefox" "pihole" "ddns" "nextcloud" "kuma" "guac" "tunnel-2" "nginx" "vault" "tdarr-1" "beeguard" )
 pve3hosts=( "tunnel-3" "tdarr-2" )
+
+copy () {
+  for host in ${1[@]}; do
+    ssh-copy-id root@$host
+  done
+}
+
+copy $pve1hosts
+copy $pve2hosts
+copy $pve3hosts
