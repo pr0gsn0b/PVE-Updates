7963790de0b48751a9d30469645ef322cc1e3bf0
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-06 14:21:16 -0600

    Update ssh-keys.sh

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -2,3 +2,4 @@
 
 pve1hosts=( "tunnel1" "avery-kuma" "cockpit" "sabnzbd" "qbit" "tdarr" "overseerr" "plex" "servarr" "minecraft" )
 pve2hosts=( "homepage" "whoogle" "firefox" "pihole" "ddns" "nextcloud" "kuma" "guac" "tunnel-2" "nginx" "vault" "tdarr-1" "beeguard" )
+pve3hosts=( "tunnel-3" "tdarr-2" )
