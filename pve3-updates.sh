b58bd6bdcfcc183b7d0115d797150e30955d71a2
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-07 13:43:43 -0600

    Update pve3-updates.sh

--- pve3-updates.sh
+++ pve3-updates.sh
@@ -4,16 +4,18 @@ pve3hosts=( "tunnel-3" "tdarr-2" )
 
 
 for host in ${pve3hosts[@]}; do
-    ssh -tt root@$host.local <<'CMD'
-    if grep -iq debian /etc/os-release > /dev/null 2>&1; then
+    ssh -tt root@$host.local <<CMD
+    if grep [[ -iq debian /etc/os-release > /dev/null 2>&1 || grep -iq ubuntu /etc/os-release > /dev/null 2>&1 ]]; then
         apt update && apt upgrade -y
     elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
         apk update && apk upgrade
     fi
 
+    if [[ $host
+
     if command -v docker > /dev/null 2>&1; then
         for ct in /home/*/; do
-            cd "$ct"
+            cd $ct
             docker compose pull
             docker compose up -d
             docker image prune -f
