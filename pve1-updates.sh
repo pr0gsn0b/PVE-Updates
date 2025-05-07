8a75c8d0969ac5cd0d3abc86b6d36a65c8541302
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-07 14:36:47 -0600

    Update pve1-updates.sh

--- pve1-updates.sh
+++ pve1-updates.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 pve1hosts=( "tunnel1" "avery-kuma" "cockpit" "sabnzbd" "qbit" "tdarr" "overseerr" "plex" "servarr" "minecraft" )
+pve2hosts=( "homepage" "whoogle" "firefox" "pihole2" "ddns" "nextcloud" "kuma" "guac" "tunnel-2" "nginx" "vault" "tdarr-1" "beeguard" )
+pve3hosts=( "tunnel-3" "tdarr-2" )
 
 
-for host in ${pve1hosts[@]}; do
+read -p "Which host would you like to update? [pve1/pve2/pve3]" choice
+
+if [[ $choice != "pve1" || $choice != "pve2" || $choice != "pve3" ]]; then
+    echo "Invalid input."
+    exit
+else
+    choice=$choice"hosts"
+    exit
+fi
+
+
+for host in ${chosts[@]}; do
     ssh -tt root@$host.local <<'CMD'
     if grep -iq debian /etc/os-release > /dev/null 2>&1; then
         apt update && apt upgrade -y
