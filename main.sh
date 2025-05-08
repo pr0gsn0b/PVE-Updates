17f8042c8cdaaec99a4fdedc48589e955249749b
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-08 11:29:04 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ if [[ $choice == "pve1" ]]; then
 elif [[ $choice == "pve2" ]]; then
     ssh root@pve2.local 'bash <(wget -qO- https://github.com/pr0gsn0b/PVE-Updates/raw/refs/heads/main/pve2-updates.sh)'
 elif [[ $choice == "pve3" ]]; then
-    ssh root@pve3.local 'bash <(wget -qO- )'
+    ssh root@pve3.local 'bash <(wget -qO- https://github.com/pr0gsn0b/PVE-Updates/raw/refs/heads/main/pve3-updates.sh)'
 else 
     echo "Invalid Input."
 fi
