9b6a7672a0c71153130b651f44dbfc7fbcc42e7d
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-08 11:21:00 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ read -p "Which host would you like to update? [pve1/pve2/pve3]" choice
 if [[ $choice == "pve1" ]]; then
     ssh root@pve1.local 'bash <(wget -qO- https://github.com/pr0gsn0b/PVE-Updates/raw/refs/heads/main/pve1-updates.sh)'
 elif [[ $choice == "pve2" ]]; then
-    ssh root@pve2.local 'bash <(wget -qO- )'
+    ssh root@pve2.local 'bash <(wget -qO- https://github.com/pr0gsn0b/PVE-Updates/raw/refs/heads/main/pve2-updates.sh)'
 elif [[ $choice == "pve3" ]]; then
     ssh root@pve3.local 'bash <(wget -qO- )'
 else 
