5b2b9029a94c96b5bed93761802775a860c8d97e
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-08 10:56:40 -0600

    Update pve1-updates.sh

--- pve1-updates.sh
+++ pve1-updates.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
 pve1hosts=( "tunnel1" "avery-kuma" "cockpit" "sabnzbd" "qbit" "tdarr" "overseerr" "plex" "servarr" "minecraft" )
-pve2hosts=( "homepage" "whoogle" "firefox" "pihole2" "ddns" "nextcloud" "kuma" "guac" "tunnel-2" "nginx" "vault" "tdarr-1" "beeguard" )
-pve3hosts=( "tunnel-3" "tdarr-2" )
 
 
-read -p "Which host would you like to update? [pve1/pve2/pve3]" choice
-
-if [[ $choice == "pve1" ]]; then
-    ssh root@pve1.local 'bash <(wget -qO- )'
-elif [[ $choice == "pve2" ]]; then
-    ssh root@pve2.local 'bash <(wget -qO- )'
-elif [[ $choice == "pve3" ]]; then
-    ssh root@pve3.local 'bash <(wget -qO- )'
-else 
-    echo "Invalid Input."
-fi
-
-
-for host in ${chosts[@]}; do
+for host in ${pve1hosts[@]}; do
     ssh -tt root@$host.local <<'CMD'
-    if grep -iq debian /etc/os-release > /dev/null 2>&1; then
+    if [[ grep -iq debian /etc/os-release > /dev/null 2>&1 || grep -iq debian /etc/os-release > /dev/null 2>&1 ]]; then
         apt update && apt upgrade -y
     elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
         apk update && apk upgrade
@@ -39,4 +24,5 @@ CMD
 done
 
 
-ssh root@pve1.local 'apt update && apt dist-upgrade -y'
+apt-get update && apt-get dist-upgrade -y
+zfs list && zpool status
