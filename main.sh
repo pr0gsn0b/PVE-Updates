e4b59ce505077af7e923071c78f1218a8e7aa8a0
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-08 10:57:51 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ pve3hosts=( "tunnel-3" "tdarr-2" )
 read -p "Which host would you like to update? [pve1/pve2/pve3]" choice
 
 if [[ $choice == "pve1" ]]; then
-    ssh root@pve1.local 'bash <(wget -qO- )'
+    ssh root@pve1.local 'bash <(wget -qO- https://github.com/pr0gsn0b/PVE-Updates/raw/refs/heads/main/pve1-updates.sh)'
 elif [[ $choice == "pve2" ]]; then
     ssh root@pve2.local 'bash <(wget -qO- )'
 elif [[ $choice == "pve3" ]]; then
@@ -17,26 +17,3 @@ else
     echo "Invalid Input."
 fi
 
-
-for host in ${chosts[@]}; do
-    ssh -tt root@$host.local <<'CMD'
-    if grep -iq debian /etc/os-release > /dev/null 2>&1; then
-        apt update && apt upgrade -y
-    elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
-        apk update && apk upgrade
-    fi
-
-    if command -v docker > /dev/null 2>&1; then
-        for ct in /home/*/; do
-            cd "$ct"
-            docker compose pull
-            docker compose up -d
-            docker image prune -f
-        done
-    fi
-    exit
-CMD
-done
-
-
-ssh root@pve1.local 'apt update && apt dist-upgrade -y'
