9468d509955da990d72b6cb0963364e8c0d7ff45
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-13 08:16:50 -0600

    Update pve3-updates.sh

--- pve3-updates.sh
+++ pve3-updates.sh
@@ -7,13 +7,16 @@ pve3hosts=( "tunnel-3" "tdarr-2" )
 for host in ${pve3hosts[@]}; do
     ssh -tt root@$host.local <<'CMD'
     if grep -iq debian /etc/os-release > /dev/null 2>&1 || grep -iq ubuntu /etc/os-release > /dev/null 2>&1; then
+        echo -e "\e[1;92mUpdating system for $(hostname)\e[0m"
         apt update && apt upgrade -y
         apt autoremove -y
     elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
+        echo -e "\e[1;92mUpdating system for $(hostname)\e[0m"
         apk update && apk upgrade
     fi
 
     if command -v docker > /dev/null 2>&1; then
+        echo -e "\e[1;94mUpdating docker images for $(hostname)\e[0m"
         for ct in /home/*/; do
             cd "$ct"
             docker compose pull
@@ -26,5 +29,6 @@ CMD
 done
 
 
+echo -e "\e[1;93mUpdating system for $(hostname)\e[0m"
 apt-get update && apt-get dist-upgrade -y
 apt autoremove -y
