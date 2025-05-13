87a3ef9232df2d1211312bb57f4823ac2c7a778a
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-05-13 08:15:08 -0600

    Update pve2-updates.sh

--- pve2-updates.sh
+++ pve2-updates.sh
@@ -7,14 +7,17 @@ pve2hosts=( "homepage" "whoogle" "firefox" "pihole2" "ddns" "nextcloud" "kuma" "
 for host in ${pve2hosts[@]}; do
     ssh -tt root@$host.local <<'CMD'
     if grep -iq debian /etc/os-release > /dev/null 2>&1 || grep -iq ubuntu /etc/os-release > /dev/null 2>&1; then
+        echo -e "\e[1;92mUpdating system for $(hostname)\e[0m"
         apt update && apt upgrade -y
         apt autoremove -y
     elif grep -iq alpine /etc/os-release > /dev/null 2>&1; then
+        echo -e "\e[1;92mUpdating system for $(hostname)\e[0m"
         apk update && apk upgrade
     fi
     
 
     if command -v docker > /dev/null 2>&1; then
+        echo -e "\e[1;94mUpdating docker images for $(hostname)\e[0m"
         for ct in /home/*/; do
             cd "$ct"
             docker compose pull
@@ -33,5 +36,5 @@ for host in ${pve2hosts[@]}; do
 CMD
 done
 
-
+echo -e "\e[1;93mUpdating system for $(hostname)\e[0m"
 apt-get update && apt-get dist-upgrade -y
